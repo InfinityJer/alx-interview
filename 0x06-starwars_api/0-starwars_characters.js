72e93025af83954fcbb3738db67b9569ebf3f451
InfinityJer <jclaris6@gmail.com>
2024-02-13 12:13:32 +0000

    0. Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,44 @@
+#!/usr/bin/nodejs
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+if (!movieId || isNaN(movieId)) {
+  console.error('Please provide a valid movie ID');
+  process.exit(1);
+}
+
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Invalid response:', response.statusCode);
+    process.exit(1);
+  }
+
+  const film = JSON.parse(body);
+  const characters = film.characters;
+
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (charError, charResponse, charBody) => {
+      if (charError) {
+        console.error('Error:', charError);
+        process.exit(1);
+      }
+
+      if (charResponse.statusCode !== 200) {
+        console.error('Invalid response:', charResponse.statusCode);
+        process.exit(1);
+      }
+
+      const character = JSON.parse(charBody);
+      console.log(character.name);
+    });
+  });
+});
