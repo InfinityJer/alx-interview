e5e88cbef84aa70afb2777106d0459208d27cf9b
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-03-06 21:32:13 +0300

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,6 +3,7 @@
 Function to calculate the perimeter of an island described in grid.
 """
 
+
 def island_perimeter(grid):
     """
     Calculates the perimeter of the island described in grid.
@@ -24,8 +25,8 @@ def island_perimeter(grid):
             if grid[i][j] == 1:  # If cell is land
                 for dx, dy in directions:
                     x, y = i + dx, j + dy
-                    if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]) or grid[x][y] == 0:
-                        perimeter += 1
+                    if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0])
+                        or grid[x][y] == 0:perimeter += 1
 
     return perimeter
 
