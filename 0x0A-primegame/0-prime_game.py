c4aa7de90be1b6f30ae24343133adcfbfd016f46
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2024-03-12 11:57:19 +0300

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,61 +1,36 @@
 #!/usr/bin/python3
-
-
-def is_prime(n):
-    """Check if a number is prime."""
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
-
-
-def generate_primes(limit):
-    """Generate all prime numbers using Sieve of Eratosthenes algorithm."""
-    primes = []
-    sieve = [True] * (limit + 1)
-    for p in range(2, limit + 1):
-        if sieve[p]:
-            primes.append(p)
-            for i in range(p * p, limit + 1, p):
-                sieve[i] = False
-    return primes
-
-
-def can_make_move(nums):
-    """Check if there are any prime numbers left to make a move."""
-    for num in nums:
-        if is_prime(num):
-            return True
-    return False
-
+"""This module contains functions determining the winner of a prime game session."""
 
 def isWinner(x, nums):
-    """Determine the winner of the game for each round."""
-    maria_wins = 0
-    ben_wins = 0
-    for n in nums:
-        if can_make_move(range(1, n + 1)):
-            # Maria always starts, so if the number is even, Ben wins
-            if n % 2 == 0:
-                ben_wins += 1
-            else:
-                maria_wins += 1
-    # Compare the number of wins for Maria and Ben
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    """Determines the winner of a prime game session with `x` rounds.
+    
+    Args:
+        x (int): The number of rounds to play.
+        nums (list): List of integers representing the game rounds.
+
+    Returns:
+        str or None: Name of winner ('Maria' or 'Ben'), or None if the winner not determined.
+    """
+    # If the number of rounds is less than 1 or nums is empty, return None
+    if x < 1 or not nums:
         return None
-
-
-# Test
-print("Winner:", isWinner(3, [4, 5, 1]))  # Output should be "Ben"
+    marias_wins, bens_wins = 0, 0
+    # Generate a list of primes with a limit of the maximum number in nums
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    # Use Sieve of Eratosthenes algorithm to mark non-prime numbers
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+    # Filter the number of primes less than n in nums for each round
+    for _, n in zip(range(x), nums):
+        primes_count = len(list(filter(lambda x: x, primes[0: n])))
+        bens_wins += primes_count % 2 == 0
+        marias_wins += primes_count % 2 == 1
+    # Determine the winner based on the number of wins
+    if marias_wins == bens_wins:
+        return None
+    return 'Maria' if marias_wins > bens_wins else 'Ben'
